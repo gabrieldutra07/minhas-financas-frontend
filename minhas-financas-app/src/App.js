50900030027d85622f6d68557c64a8f0962d331a
Gabriel Dutra <gabrielcbdutra@gmail.com>
2022-09-20 20:53:48 -0300

    brincando com alteração de estado dentro de um input

--- minhas-financas-app/src/App.js
+++ minhas-financas-app/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    state = {
+      nome: ''
+    }
+
+    render() {
+      return (
+        <div>
+            <label>Nome:</label>
+            <input type="text" value={this.state.nome} 
+            onChange={(e) => this.setState({nome: e.target.value})} />
+
+            O nome digitado foi: {this.state.nome}
+        </div>
+      )
+    }
 }
 
-export default App;
+
+export default App
