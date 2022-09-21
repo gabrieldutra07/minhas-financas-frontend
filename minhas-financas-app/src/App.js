47ce8f567b2b0d00fd4a4f0d9f2fff8f2305422b
Gabriel Dutra <gabrielcbdutra@gmail.com>
2022-09-20 21:07:51 -0300

    utilizando alteração de estado para realizar uma soma nos inputs

--- minhas-financas-app/src/App.js
+++ minhas-financas-app/src/App.js
@@ -3,17 +3,29 @@ import React from 'react'
 class App extends React.Component {
 
     state = {
-      nome: ''
+      numero1: null,
+      numero2: null,
+      resultado: null
+    }
+
+    somar = () => {
+        const resultado = parseInt(this.state.numero1) + parseInt(this.state.numero2)
+        this.setState({resultado: resultado})
     }
 
     render() {
       return (
         <div>
-            <label>Nome:</label>
-            <input type="text" value={this.state.nome} 
-            onChange={(e) => this.setState({nome: e.target.value})} />
+            <label>Número:</label>
+            <input type="text" value={this.state.numero1} 
+            onChange={(e) => this.setState({numero1: e.target.value})} /><br></br>
+            <input type="text" value={this.state.numero2} 
+            onChange={(e) => this.setState({numero2: e.target.value})} />
+            
+            <button 
+            onClick={this.somar}>Somar</button><br></br>
 
-            O nome digitado foi: {this.state.nome}
+            O resultado é: {this.state.resultado}
         </div>
       )
     }
