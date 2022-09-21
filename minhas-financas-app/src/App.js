c337f4d3d87aac023227299c5d4991e9e75ef0a8
Gabriel Dutra <gabrielcbdutra@gmail.com>
2022-09-20 22:49:34 -0300

    correção (adição do return)

--- minhas-financas-app/src/App.js
+++ minhas-financas-app/src/App.js
@@ -4,10 +4,12 @@ import 'bootswatch/dist/flatly/bootstrap.css'
 class App extends React.Component {
 
   render() {
+    return (
       <div>
         <button type="button" className="btn btn-primary">Primary</button>
       </div>
-    }
+    )
+  }
 }
 
 export default App
