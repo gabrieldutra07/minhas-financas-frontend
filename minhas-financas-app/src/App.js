abe8f959df0d294c169273a3fa48af16f78ec6fd
Gabriel Dutra <gabrielcbdutra@gmail.com>
2022-09-20 22:47:25 -0300

    importado as dependencias do bootswatch

--- minhas-financas-app/src/App.js
+++ minhas-financas-app/src/App.js
@@ -1,35 +1,13 @@
 import React from 'react'
+import 'bootswatch/dist/flatly/bootstrap.css'
 
 class App extends React.Component {
 
-    state = {
-      numero1: null,
-      numero2: null,
-      resultado: null
-    }
-
-    somar = () => {
-        const resultado = parseInt(this.state.numero1) + parseInt(this.state.numero2)
-        this.setState({resultado: resultado})
-    }
-
-    render() {
-      return (
-        <div>
-            <label>Número:</label>
-            <input type="text" value={this.state.numero1} 
-            onChange={(e) => this.setState({numero1: e.target.value})} /><br></br>
-            <input type="text" value={this.state.numero2} 
-            onChange={(e) => this.setState({numero2: e.target.value})} />
-            
-            <button 
-            onClick={this.somar}>Somar</button><br></br>
-
-            O resultado é: {this.state.resultado}
-        </div>
-      )
+  render() {
+      <div>
+        <button type="button" className="btn btn-primary">Primary</button>
+      </div>
     }
 }
-
 
 export default App
